f7fffaeced6d6ee8dee115b4e543e767210391df
fabriciodisalvo <fabriciodisalvo@gmail.com>
2020-06-11 13:39:19 -0300

    Add self.moves_made.add(), self.mark_safe(cell)
    In MinesweeperAI class, add_knowledge(): Added
    moves_made.add(cell) to add the cell to the moves made set.
    Added self.mark_safe(cell) to mark the cell as safe in this
    method and mark as safe in any Sentence in Knowledge.

--- project1/minesweeper/minesweeper.py
+++ project1/minesweeper/minesweeper.py
@@ -192,7 +192,8 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
 
     def make_safe_move(self):
         """
